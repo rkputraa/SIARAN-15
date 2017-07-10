c571f0cef9ff1459e29be74bc9e6c0a2e2e7334a
rkputraa <rkputraa@gmail.com>
2017-07-10 16:37:28 +0700

    commit template admin

---
+++ template admin/content.php
@@ -0,0 +1,51 @@
+
+	<div id="rightmid" class="col-md-10">
+		<h3>Data Menu WAR Resto</h3>
+		<h5><a href="admintambahitemmenu.html"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Create Jenis Menu</a></h5>
+		<table  class="table table-bordered" width="100% "border="3" cellspacing="0" cellpadding="10" border-color="white">
+		<tr>
+		<th align="center">No</th>
+		<th>Kode Item</th>
+		<th>Nama Item</th>
+		<th>Harga</th>
+		<th>Action</th>
+		</tr>
+		<tr>
+		<td>1</td>
+		<td>FF001</td>
+		<td>Ayam Cabe Ijo</td>
+		<td>18000</td>
+		<td> <a href="detailitemmenuadmin.html">
+			<button type="button" class="btn btn-info">Lihat</button>
+			<a href="edititemmenuadmin.html">
+			<button type="button" class="btn btn-warning">Edit</button>
+		<a href="#">
+			<button type="button" class="btn btn-danger" onclick="tombol();">Hapus</button></td>
+		</tr>
+		<tr>
+		<td>2</td>
+		<td>DD001</td>
+		<td>Teh Es</td>
+		<td>3000</td>
+		<td> <a href="detailitemmenuadmin.html">
+			<button type="button" class="btn btn-info">Lihat</button>
+			<a href="edititemmenuadmin.html">
+			<button type="button" class="btn btn-warning">Edit</button>
+		<a href="#">
+			<button type="button" class="btn btn-danger" onclick="tombol();">Hapus</button></td>
+		</tr>
+		<tr>
+		<td>3</td>
+		<td>RR001</td>
+		<td>French Fries</td>
+		<td>9000</td>
+		<td> <a href="detailitemmenuadmin.html">
+			<button type="button" class="btn btn-info">Lihat</button>
+			<a href="edititemmenuadmin.html">
+			<button type="button" class="btn btn-warning">Edit</button>
+		<a href="#">
+			<button type="button" class="btn btn-danger" onclick="tombol();">Hapus</button></td>
+		</tr>
+		</table>
+
+	</div>
