71da31b04f9c49458333db314e91731b136a5477
alif <juno7alif@gmail.com>
2017-07-10 16:52:01 +0700

    h

---
+++ administrator/template admin/sidebar.php
@@ -0,0 +1,22 @@
+
+<div class="row">
+	<div class="col-md-2" id="leftbar">
+				<ul class="nav nav-pills nav-stacked" role="tablist">
+				    <li class="active"><a href="#">Halaman Admin</a></li>
+				    <li><a href="olahuseradmin.html"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>Olah Data User</a></li>
+					<li><a href="olahgudangadmin.html"><span class="glyphicon glyphicon-apple" aria-hidden="true"></span> Data Barang</a>
+					<ul>
+					<li><a href="adminbrg.html"><span class="glyphicon glyphicon-file" aria-hidden="true"></span> Data Barang</a></li>
+					<li><a href="adminbrgmsk.html"><span class="glyphicon glyphicon-file" aria-hidden="true"></span> Barang Masuk</a></li>
+					<li><a href="adminbrgklr.html"><span class="glyphicon glyphicon-file" aria-hidden="true"></span> Barang Keluar</a></li>
+					</ul>
+					</li>
+					<br><br><br><li><a href="olahmemberadmin.html"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Data Member</a></li>
+					 <li><a href="olahmenuadmin.html"><span class="glyphicon glyphicon-file" aria-hidden="true"></span> Olah Data Menu</a>
+					<ul>
+					<li><a href="olahitemmenuadmin.html"><span class="glyphicon glyphicon-file" aria-hidden="true"></span> Item Menu</a></li>
+					<li><a href="olahmenuadmin.html"><span class="glyphicon glyphicon-file" aria-hidden="true"></span> Jenis Menu</a></li>
+					</li>
+					</ul>
+				</ul>				
+			</div>
