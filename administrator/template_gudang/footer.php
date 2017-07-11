a2784b97c4aebcd44187432ff67f22a210f274d3
rkputraa <rkputraa@gmail.com>
2017-07-11 16:53:17 +0700

    commit logistik

---
+++ administrator/template_gudang/footer.php
@@ -0,0 +1,13 @@
+
+	<div id="footer" class="col-md-12">
+		<h5>Copyright &copy; Alif, Rachmat, Wilda | Sistem Informasi 2015</h5>
+	</div>
+	<script>
+		function tombol() {
+		confirm("Apakah anda setuju?");
+		}
+	</script>
+</div>
+</div>
+</body>
+</html>
